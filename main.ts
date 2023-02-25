6372635f0d93a6f9f38f0e73599752a67a062331
flolu <loflude@gmail.com>
2023-02-25 11:19:18 +0100

    ♻️ update line count on load

--- main.ts
+++ main.ts
@@ -3,32 +3,38 @@ import { Plugin } from "obsidian";
 export default class ExamplePlugin extends Plugin {
 	statusBarElement: HTMLSpanElement;
 
-	private updateLineCount(fileContent?: string) {
-		const count = fileContent ? fileContent.split(/\r\n|\r|\n/).length : 0;
-		const linesWord = count === 1 ? "line" : "lines";
-		this.statusBarElement.textContent = `${count} ${linesWord}`;
-	}
-
 	onload() {
 		this.statusBarElement = this.addStatusBarItem().createEl("span");
 
-		this.app.workspace.on("editor-change", async (editor) => {
+		this.readActiveFileAndUpdateLineCount();
+
+		this.app.workspace.on("editor-change", (editor) => {
 			const content = editor.getDoc().getValue();
 			this.updateLineCount(content);
 		});
 
-		this.app.workspace.on("active-leaf-change", async () => {
-			const file = this.app.workspace.getActiveFile();
-			if (file) {
-				const content = await this.app.vault.read(file);
-				this.updateLineCount(content);
-			} else {
-				this.updateLineCount(undefined);
-			}
+		this.app.workspace.on("active-leaf-change", () => {
+			this.readActiveFileAndUpdateLineCount();
 		});
 	}
 
 	onunload() {
 		this.statusBarElement.remove();
+	}
+
+	private async readActiveFileAndUpdateLineCount() {
+		const file = this.app.workspace.getActiveFile();
+		if (file) {
+			const content = await this.app.vault.read(file);
+			this.updateLineCount(content);
+		} else {
+			this.updateLineCount(undefined);
+		}
+	}
+
+	private updateLineCount(fileContent?: string) {
+		const count = fileContent ? fileContent.split(/\r\n|\r|\n/).length : 0;
+		const linesWord = count === 1 ? "line" : "lines";
+		this.statusBarElement.textContent = `${count} ${linesWord}`;
 	}
 }
